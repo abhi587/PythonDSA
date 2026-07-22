87d3e94c06c4b26125ff241a35f16b0a18dc7f9d
abhi587 <abhishek.pawan14@gmail.com>
2026-07-22 21:27:55 +0530

    reverse

--- reverseList.py
+++ reverseList.py
@@ -10,3 +10,21 @@ def reverse(arr):
     print(result)
 
 reverse(arr)
+
+
+def reverse_list(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        # Swap elements
+        arr[left], arr[right] = arr[right], arr[left]
+
+        left += 1
+        right -= 1
+
+    return arr
+
+
+arr = [1, 2, 3, 4, 5]
+print(reverse_list(arr))
