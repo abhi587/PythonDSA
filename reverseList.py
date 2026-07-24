5157012bf067c225e728af7c68651f6a17f3886b
abhi587 <abhishek.pawan14@gmail.com>
2026-07-24 21:11:38 +0530

    new

--- reverseList.py
+++ reverseList.py
@@ -1,23 +1,25 @@
-arr = [1,2,34,5,3,23,9]
+# arr = [1,2,34,5,3,23,9]
+
+# def reverse(arr):
+    
+#     result = []
+
+#     for i in range(len(arr)-1,-1,-1):
+#         result.append(arr[i])
+
+#     print(result)
+
+# reverse(arr)
+
+
+# using two pointer reversing a list 
 
 def reverse(arr):
-    
-    result = []
-
-    for i in range(len(arr)-1,-1,-1):
-        result.append(arr[i])
-
-    print(result)
-
-reverse(arr)
-
-
-def reverse_list(arr):
-    left = 0
-    right = len(arr) - 1
+    left = 0 
+    right = len(arr)-1
 
     while left < right:
-        # Swap elements
+
         arr[left], arr[right] = arr[right], arr[left]
 
         left += 1
@@ -25,6 +27,4 @@ def reverse_list(arr):
 
     return arr
 
-
-arr = [1, 2, 3, 4, 5]
-print(reverse_list(arr))
+print(reverse([1,2,3,4,5]))
