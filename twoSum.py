2ebd8459139817c42d007579a6c451ba208663f6
abhi587 <abhishek.pawan14@gmail.com>
2026-07-28 22:03:06 +0530

    longestCommonPrefix

--- twoSum.py
+++ twoSum.py
@@ -1,6 +1,9 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i!=j and nums[i]+nums[j] == target:
-                    return [i,j]
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i!=j and nums[i]+nums[j] == target:
+                return [i,j]
+
+
+print(twoSum([12,45,6,7,2],14))
