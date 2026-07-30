252119e5145ef4c340bb0945acfed05bb0c164ab
abhi587 <abhishek.pawan14@gmail.com>
2026-07-30 14:16:30 +0530

    majorityElement

--- majorityElement.py
+++ majorityElement.py
@@ -5,6 +5,22 @@ def majorityElement(nums):
         if count > n // 2:
             return num
 
+
+def majorityElement(nums):
+    candidate = None
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+
+        if num == candidate:
+            count += 1
+        else:
+            count -= 1
+
+    return candidate
+
 nums = [5,5,1,1,1,5,5]
 
 print(majorityElement(nums))
