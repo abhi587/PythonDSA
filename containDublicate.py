e1c3064dafa182491e98ed14d42df1d2fd954113
abhi587 <abhishek.pawan14@gmail.com>
2026-08-05 23:24:04 +0530

    removeDublicate

--- containDublicate.py
+++ containDublicate.py
@@ -1,14 +1,40 @@
-def hasDublicate(arr):
-    hashset = set()
+## Below code return true and false if dublcate present in the list 
 
-    for n in arr:
-        if n in hashset:
-            return True
-        hashset.add(n)
+# def hasDublicate(arr):
+#     hashset = set()
 
-    return False
+#     for n in arr:
+#         if n in hashset:
+#             return True
+#         hashset.add(n)
+
+#     return False
     
 
-arr = [1,2,3,1]
+# arr = [1,2,3,1]
 
-print(hasDublicate(arr))
+# print(hasDublicate(arr))
+
+
+# Below code Remove the Dublicate elements and return the final list 
+
+arr = [1,1,2,2,3,4,4]
+
+def removeDuplicate(arr):
+    if not arr:
+        return None
+
+    write = 0
+
+    for read in range(1,len(arr)):
+        if arr[read] != arr[write]:
+            write += 1
+
+            arr[write] = arr[read]
+
+    return write + 1
+
+k = removeDuplicate(arr)
+
+print(k)
+print(arr[:k])
