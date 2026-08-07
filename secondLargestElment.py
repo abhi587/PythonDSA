4b5e5d309ba6efbe9d49b3ddb20cb2871fbefd79
abhi587 <abhishek.pawan14@gmail.com>
2026-08-07 20:29:45 +0530

    secondLargest

---
+++ secondLargestElment.py
@@ -0,0 +1,19 @@
+def second_largest(arr):
+
+    largest = float("-inf")
+    second = float("-inf")
+
+    for num in arr:
+
+        if num > largest:
+            second = largest
+            largest = num
+
+        elif largest > num > second:
+            second = num
+
+    return second
+
+arr = [10, 5, 8, 20, 20]
+
+print(second_largest(arr))
