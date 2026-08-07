519e161a6e027d75cbb5357828f8773f4a56742b
abhi587 <abhishek.pawan14@gmail.com>
2026-08-07 20:40:11 +0530

    returnDublicateELement

--- containDublicate.py
+++ containDublicate.py
@@ -18,23 +18,46 @@
 
 # Below code Remove the Dublicate elements and return the final list FOR SORTED ARRAY
 
-arr = [1,1,2,2,3,4,4]
+# arr = [1,1,2,2,3,4,4]
 
-def removeDuplicate(arr):
-    if not arr:
-        return None
+# def removeDuplicate(arr):
+#     if not arr:
+#         return None
 
-    write = 0
+#     write = 0
 
-    for read in range(1,len(arr)):
-        if arr[read] != arr[write]:
-            write += 1
+#     for read in range(1,len(arr)):
+#         if arr[read] != arr[write]:
+#             write += 1
 
-            arr[write] = arr[read]
+#             arr[write] = arr[read]
 
-    return write + 1
+#     return write + 1
 
-k = removeDuplicate(arr)
+# k = removeDuplicate(arr)
 
-print(k)
-print(arr[:k])
+# print(k)
+# print(arr[:k])
+
+
+# Below code is to retur the dublicate element from the list 
+
+
+arr = [1,3,4,2,2,3]
+
+def return_dub(arr):
+    
+    seen = set()
+    result = []
+
+    for num in arr:
+
+        if num in seen:
+            result.append(num)
+
+        seen.add(num)
+    
+    return result
+
+
+print(return_dub(arr))
