74588cab7ce9624b03e3e8a3360b90123df76f06
abhi587 <abhishek.pawan14@gmail.com>
2026-08-11 20:58:14 +0530

    donE

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,17 +1,22 @@
-def product_except_self(nums):
+def productExceptSelf(nums):
     n = len(nums)
     result = [1] * n
 
+    # Product of all elements to the left
     prefix = 1
-
     for i in range(n):
         result[i] = prefix
         prefix *= nums[i]
 
+    # Product of all elements to the right
     suffix = 1
-
     for i in range(n - 1, -1, -1):
         result[i] *= suffix
         suffix *= nums[i]
 
     return result
+
+
+nums = [1, 2, 3, 4]
+
+print(productExceptSelf(nums))
