7f34e808f388610194fd8eeb69c06f0c4de0403f
abhi587 <abhishek.pawan14@gmail.com>
2026-08-11 20:54:18 +0530

    productofArrayExceptSelf

---
+++ ProductListExcSelf.py
@@ -0,0 +1,17 @@
+def productExceptSelf(nums):
+    n = len(nums)
+    result = [1] * n
+
+    # Product of all elements to the left
+    prefix = 1
+    for i in range(n):
+        result[i] = prefix
+        prefix *= nums[i]
+
+    # Product of all elements to the right
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
