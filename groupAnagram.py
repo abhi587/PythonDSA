c944be1280a03d75fab8fc041101a274358294d1
abhi587 <abhishek.pawan14@gmail.com>
2026-08-12 20:43:51 +0530

    groupAnagram

---
+++ groupAnagram.py
@@ -0,0 +1,14 @@
+def group_anagrams(words):
+
+    groups = {}
+
+    for word in words:
+
+        key = "".join(sorted(word))
+
+        if key not in groups:
+            groups[key] = []
+
+        groups[key].append(word)
+
+    return list(groups.values())
