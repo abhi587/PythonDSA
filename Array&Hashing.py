4b7eaa53feec1c5bcb3972e8663fe95d56d602c6
abhi587 <abhishek.pawan14@gmail.com>
2026-08-25 19:54:15 +0530

    done

--- Array&Hashing.py
+++ Array&Hashing.py
@@ -5,6 +5,19 @@
 #         return nums+numsA
 
 
+# 2. comtains duplicate
+
+# class Solution:
+#         def hasDuplicate(self, nums: List[int]) -> bool:
+#                 hashset = set()
+
+#                 for n in nums:
+#                         if n in hashset:
+#                                 return True
+#                         hashset.add(n)
+#                 return False
+
+
 
 # class Solution:
 #     def sortColors(self, nums: List[int]) -> None:
