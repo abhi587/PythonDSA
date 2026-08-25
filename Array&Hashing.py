ed3c08bda2986de9ed0ac5db5d614039b800eac9
abhi587 <abhishek.pawan14@gmail.com>
2026-08-25 19:53:26 +0530

    done

--- Array&Hashing.py
+++ Array&Hashing.py
@@ -1,20 +1,28 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        low = 0 
-        mid = 0 
-        high = len(nums) -1
+# 1 . getConcatenation
 
-        while mid <= high:
-            if nums[mid] == 0:
-                nums[low],nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1 
-            elif nums[mid] == 1:
-                mid +=1
-            else:
-                nums[mid], nums[high] = nums[high], nums[mid]
-                high -= 1
+# class Solution:
+#     def getConcatenation(self, nums: List[int]) -> List[int]:
+#         return nums+numsA
+
+
+
+# class Solution:
+#     def sortColors(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         low = 0 
+#         mid = 0 
+#         high = len(nums) -1
+
+#         while mid <= high:
+#             if nums[mid] == 0:
+#                 nums[low],nums[mid] = nums[mid], nums[low]
+#                 low += 1
+#                 mid += 1 
+#             elif nums[mid] == 1:
+#                 mid +=1
+#             else:
+#                 nums[mid], nums[high] = nums[high], nums[mid]
+#                 high -= 1
             
