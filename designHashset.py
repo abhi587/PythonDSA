afd8d14d4220a5f0afaadc4d6fe616786bf18251
abhi587 <abhishek.pawan14@gmail.com>
2026-08-26 22:26:36 +0530

    done

--- designHashset.py
+++ designHashset.py
@@ -1,28 +1,28 @@
-class MyHashSet:
+# class MyHashSet:
 
-    def __init__(self):
-        self.data = []
+#     def __init__(self):
+#         self.data = []
 
-    def add(self, key: int) -> None:
-        if key not in self.data:
-            self.data.append(key)
+#     def add(self, key: int) -> None:
+#         if key not in self.data:
+#             self.data.append(key)
 
-    def remove(self, key: int) -> None:
-        if key in self.data:
-            self.data.remove(key)
+#     def remove(self, key: int) -> None:
+#         if key in self.data:
+#             self.data.remove(key)
 
-    def contains(self, key: int) -> bool:
-        return key in self.data
-
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+#     def contains(self, key: int) -> bool:
+#         return key in self.data
 
 
-Input: ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
-[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+# # Your MyHashSet object will be instantiated and called as such:
+# # obj = MyHashSet()
+# # obj.add(key)
+# # obj.remove(key)
+# # param_3 = obj.contains(key)
 
-Output: [null, null, null, true, false, null, true, null, false]
+
+# Input: ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+# [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+
+# Output: [null, null, null, true, false, null, true, null, false]
