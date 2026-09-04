99bbcb1c8b60eceeb3aa36b529dab2c760eaa032
abhi587 <abhishek.pawan14@gmail.com>
2026-09-04 19:50:53 +0530

    dne

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,14 +1,14 @@
 
-def longestCommonPrefix(strs):
-    res = ""
+# def longestCommonPrefix(strs):
+#     res = ""
 
-    for i in range(len(strs[0])):
-            for s in strs:
-                    if i == len(s) or s[i] != strs[0][i]:
-                            return res
-            res += strs[0][i]
+#     for i in range(len(strs[0])):
+#             for s in strs:
+#                     if i == len(s) or s[i] != strs[0][i]:
+#                             return res
+#             res += strs[0][i]
 
-    return res                
+#     return res                
 
-print(longestCommonPrefix(["dance","dag","danger","damage"]
-))
+# print(longestCommonPrefix(["dance","dag","danger","damage"]
+# ))
