261ea6b87f6a4cb6208bc0b73cf8ee20d23fb2ba
abhi587 <abhishek.pawan14@gmail.com>
2026-09-08 19:31:02 +0530

    done

--- moveZeroToEnd.py
+++ moveZeroToEnd.py
@@ -1,21 +1,21 @@
-arr = [0,1,0,3,12]
+# arr = [0,1,0,3,12]
 
-# output = [1,3,12,0,0]
+# # output = [1,3,12,0,0]
 
-def move_zero(arr):
-    if not arr:
-        return None
+# def move_zero(arr):
+#     if not arr:
+#         return None
     
-    write = 0 
+#     write = 0 
 
-    for read in range(len(arr)):
+#     for read in range(len(arr)):
 
-        if arr[read] != 0:
+#         if arr[read] != 0:
 
-            arr[write], arr[read] = arr[read], arr[write]
+#             arr[write], arr[read] = arr[read], arr[write]
 
-            write += 1
+#             write += 1
 
-    return arr
+#     return arr
 
-print(move_zero(arr))
+# print(move_zero(arr))
